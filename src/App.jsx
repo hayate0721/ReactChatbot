d5bc5e06e4772f6a8aaddb06b197b7caab2e8719
hayate0721 <h08066802495@gmail.com>
2024-06-17 12:18:49 -0700

    Added typing indicator

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
-import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react"
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
 function App() {
+  const [typing, setTyping] = useState(false);
   const [messages, setMessages] = useState([
     {
       message: "Hello, I am ChatGPT!",
@@ -20,10 +19,13 @@ function App() {
       direction: "outgoing"
     }
 
-    const newMessages = [...messages, newMessage]; // all the old message + the new one
+    const newMessages = [...messages, newMessage]; // all the old message + the new message
 
     // update our message state
     setMessages(newMessages);
+
+    // set a typing indicator (chatgpt is typing)
+    setTyping(true);
 
     // process message to chatGPT (send it over and see the response)
   }
@@ -33,7 +35,9 @@ function App() {
       <div style={{ position: "relative", height: "800px", width: "700px"}}>
         <MainContainer>
           <ChatContainer>
-            <MessageList>
+            <MessageList
+            typingIndicator={typing? <TypingIndicator content = "ChatGPT is typing"/> : null}
+            >
               {messages.map((message, i) => {
                 return <Message key={i} model={message} />
               })}
